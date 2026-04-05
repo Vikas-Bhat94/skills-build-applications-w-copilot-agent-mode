e3f1fac64e6e8faf7cd1e0e444e5e7abac3aec2a
Vikas-Bhat94 <vikas.bhat@ltimindtree.com>
2026-04-05 08:34:56 +0000

    Update settings and views to support dynamic ALLOWED_HOSTS and API base URL for Codespaces

--- octofit-tracker/backend/octofit_tracker/views.py
+++ octofit-tracker/backend/octofit_tracker/views.py
@@ -30,10 +30,17 @@ class WorkoutViewSet(viewsets.ModelViewSet):
 
 @decorators.api_view(['GET'])
 def api_root(request, format=None):
+
+	import os
+	codespace_name = os.environ.get('CODESPACE_NAME')
+	if codespace_name:
+		base_url = f"https://{codespace_name}-8000.app.github.dev/api/"
+	else:
+		base_url = '/api/'
 	return response.Response({
-		'users': '/api/users/',
-		'teams': '/api/teams/',
-		'activities': '/api/activities/',
-		'leaderboard': '/api/leaderboard/',
-		'workouts': '/api/workouts/',
+		'users': f'{base_url}users/',
+		'teams': f'{base_url}teams/',
+		'activities': f'{base_url}activities/',
+		'leaderboard': f'{base_url}leaderboard/',
+		'workouts': f'{base_url}workouts/',
 	})
